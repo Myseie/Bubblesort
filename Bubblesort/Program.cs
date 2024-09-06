2834364dd33e88d65e53ab3e7e1625969e714f77
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:42:46 +0200

    Add files via upload

---
+++ Bubblesort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+int[] number = { 5, 2, 3, 1, 9, 33, 11, 22  };
+
+
+for (int i = 0; i < number.Length - 1; i++)
+{
+    for (int j = 0; j < number.Length - 1 - i; j++)
+    {
+        if (number[i] == number[j])
+        {
+            int temp = number[j];
+            number[j] = number[i + 1];
+            number[j + 1] = temp;
+
+        }
+    }
+}
+
+Console.WriteLine("Sorterad array");
+foreach (int num in number)
+{
+    Console.WriteLine(num +" "); 
+}
